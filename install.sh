992bfd07eb8bbf39182936b5a20f317e42cfb53f
William Ting <william.h.ting@gmail.com>
2012-02-17 16:22:46 -1000

    differentiate install based on username

--- install.sh
+++ install.sh
@@ -2,17 +2,23 @@
 
 timestamp=`/usr/bin/env date +%s`
 if [ -d ~/.vim ] || [ -h ~/.vim ]; then
-	echo -e "\nExisting ~/.vim/, moving to backup location...\n"
+	echo -e "\nExisting ~/.vim/, moving to backup location ...\n"
 	mv -v ~/.vim ~/.vim-backup-${timestamp}
 fi
 
 if [ -f ~/.vimrc ] || [ -h ~/.vimrc ]; then
-	echo -e "\nExisting ~/.vimrc, moving to backup location...\n"
+	echo -e "\nExisting ~/.vimrc, moving to backup location ...\n"
 	mv -v ~/.vimrc ~/.vimrc-backup-${timestamp}
 fi
 
-echo -e "\nCloning dotvim repository...\n"
-/usr/bin/env git clone https://github.com/wting/dotvim.git/ ~/.vim/
+echo -e "\nCloning dotvim repository into ~/.vim/ ...\n"
+if [ `whoami` == "ting" ]; then
+	# private read/write repo
+	/usr/bin/env git clone git@github.com:wting/dotvim.git ~/.vim/
+else
+	# public read only repo
+	/usr/bin/env git clone https://github.com/wting/dotvim.git/ ~/.vim/
+fi
 
-echo -e "\nSymlinking ~/.vimrc...\n"
+echo -e "\nSymlinking ~/.vimrc ...\n"
 ln -s ~/.vim/.vimrc ~/.vimrc
