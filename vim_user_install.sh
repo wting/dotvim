4188a7e6dd7c67a8c1a91f37ed85b16196e4bf8c
William Ting <io@williamting.com>
2016-06-02 22:56:57 -0700

    Surround vars with brackets.

--- vim_user_install.sh
+++ vim_user_install.sh
@@ -1,33 +1,35 @@
 #!/bin/bash
 # Ubuntu requirements: ncurses-dev python-dev
 # Cygwin requirements: libncurses-devel python-dev
+
+# set -o errexit -o nounset -o pipefail
+
 email="io@williamting.com"
 tmp=$HOME/.vim/tmp
-src="$tmp/src"
-old_dst=$HOME/bin/vim7.4
+src="${tmp}/src"
 dst=$HOME/bin/vim-7.4
 python27_config=/usr/lib/python2.7/config
 python26_config=/usr/lib/python2.6/config
 python_config=
 
-if [ -d "$python27_config" ]; then
-    python_config=$python27_config
-elif [ -d "$python26_config" ]; then
-    python_config=$python26_config
+if [ -d "${python27_config}" ]; then
+    python_config=${python27_config}
+elif [ -d "${python26_config}" ]; then
+    python_config=${python26_config}
 else
     echo "Could not find Python config directory."
     exit 1
 fi
 
-[[ -d "$tmp" ]] || mkdir -vp $tmp
-[[ -d "$tmp/vim_source" ]] && rm -fr "$tmp/vim_source"
+[[ -d "${tmp}" ]] || mkdir -vp ${tmp}
+[[ -d "${tmp}/vim_source" ]] && rm -fr "${tmp}/vim_source"
 
-if [[ -d "$src" ]]; then
-    cd $src
+if [[ -d "${src}" ]]; then
+    cd ${src}
     git fetch origin --prune && git reset --hard origin/master || exit 1
 else
-    git clone https://github.com/vim/vim.git $src || exit 1
-    cd $src
+    git clone https://github.com/vim/vim.git ${src} || exit 1
+    cd ${src}
 fi
 
 ./configure \
@@ -36,21 +38,20 @@ fi
     --enable-multibyte \
     --enable-cscope \
     --enable-pythoninterp \
-    --with-python-config-dir=$python_config \
-    --prefix="$dst" \
+    --with-python-config-dir=${python_config} \
+    --prefix="${dst}" \
     --with-compiledby=$email || exit 1
 
-[[ -d "$dst" ]] || mkdir -vp $dst
+[[ -d "${dst}" ]] || mkdir -vp ${dst}
 
 make -j$(nproc) && make install || exit 1
 
-ln -sfv $dst/bin/vim     $HOME/bin
-ln -sfv $dst/bin/vimdiff $HOME/bin
+chmod +x ${dst}
+ln -sfv ${dst}/bin/vim     $HOME/bin
+ln -sfv ${dst}/bin/vimdiff $HOME/bin
 
 echo
-$dst/bin/vim --version | head | grep --color "7.."
-$dst/bin/vim --version | head | grep --color "1-.*"
-$dst/bin/vim --version | grep -E --color "[-+]python[0-9]?"
-echo -e "\ninstalled to: $dst\n"
-
-[[ -d ${old_dst} ]] && rm -fr ${old_dst}
+${dst}/bin/vim --version | head | grep --color "7.."
+${dst}/bin/vim --version | head | grep --color "1-.*"
+${dst}/bin/vim --version | grep -E --color "[-+]python[0-9]?"
+echo -e "\ninstalled to: ${dst}\n"
